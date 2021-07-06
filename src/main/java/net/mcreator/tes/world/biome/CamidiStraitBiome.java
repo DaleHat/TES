d559517501a858371f564cdb39f8f70d4346f5ce
DaleHat <d__c_@192.168.1.232>
2021-07-06 03:47:50 -0500

    added major water bodies

---
+++ src/main/java/net/mcreator/tes/world/biome/CamidiStraitBiome.java
@@ -0,0 +1,55 @@
+
+package net.mcreator.tes.world.biome;
+
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.event.RegistryEvent;
+
+import net.minecraft.world.gen.surfacebuilders.SurfaceBuilderConfig;
+import net.minecraft.world.gen.surfacebuilders.SurfaceBuilder;
+import net.minecraft.world.gen.feature.structure.StructureFeatures;
+import net.minecraft.world.biome.MobSpawnInfo;
+import net.minecraft.world.biome.DefaultBiomeFeatures;
+import net.minecraft.world.biome.BiomeGenerationSettings;
+import net.minecraft.world.biome.BiomeAmbience;
+import net.minecraft.world.biome.Biome;
+import net.minecraft.block.Blocks;
+
+import net.mcreator.tes.TesModElements;
+
+@TesModElements.ModElement.Tag
+public class CamidiStraitBiome extends TesModElements.ModElement {
+	public static Biome biome;
+	public CamidiStraitBiome(TesModElements instance) {
+		super(instance, 142);
+		FMLJavaModLoadingContext.get().getModEventBus().register(new BiomeRegisterHandler());
+	}
+	private static class BiomeRegisterHandler {
+		@SubscribeEvent
+		public void registerBiomes(RegistryEvent.Register<Biome> event) {
+			if (biome == null) {
+				BiomeAmbience effects = new BiomeAmbience.Builder().setFogColor(12638463).setWaterColor(4159204).setWaterFogColor(329011)
+						.withSkyColor(7972607).withFoliageColor(10387789).withGrassColor(9470285).build();
+				BiomeGenerationSettings.Builder biomeGenerationSettings = new BiomeGenerationSettings.Builder()
+						.withSurfaceBuilder(SurfaceBuilder.DEFAULT.func_242929_a(new SurfaceBuilderConfig(Blocks.SAND.getDefaultState(),
+								Blocks.GRAVEL.getDefaultState(), Blocks.GRAVEL.getDefaultState())));
+				biomeGenerationSettings.withStructure(StructureFeatures.STRONGHOLD);
+				biomeGenerationSettings.withStructure(StructureFeatures.MINESHAFT);
+				biomeGenerationSettings.withStructure(StructureFeatures.MONUMENT);
+				biomeGenerationSettings.withStructure(StructureFeatures.SHIPWRECK);
+				biomeGenerationSettings.withStructure(StructureFeatures.OCEAN_RUIN_WARM);
+				DefaultBiomeFeatures.withCavesAndCanyons(biomeGenerationSettings);
+				DefaultBiomeFeatures.withOverworldOres(biomeGenerationSettings);
+				MobSpawnInfo.Builder mobSpawnInfo = new MobSpawnInfo.Builder().isValidSpawnBiomeForPlayer();
+				biome = new Biome.Builder().precipitation(Biome.RainType.RAIN).category(Biome.Category.OCEAN).depth(-1f).scale(0.2f).temperature(0.5f)
+						.downfall(0.5f).setEffects(effects).withMobSpawnSettings(mobSpawnInfo.copy())
+						.withGenerationSettings(biomeGenerationSettings.build()).build();
+				event.getRegistry().register(biome.setRegistryName("tes:camidi_strait"));
+			}
+		}
+	}
+	@Override
+	public void init(FMLCommonSetupEvent event) {
+	}
+}
